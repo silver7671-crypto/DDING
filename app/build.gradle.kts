d8d6bda1cca4201c2818f18613ead54547f3de1b
silver7671-crypto <silver7671@gmail.com>
2026-07-24 21:20:03 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ddeeng.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
     buildTypes {
         release { isMinifyEnabled = false }
